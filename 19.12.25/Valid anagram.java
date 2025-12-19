799c8a0c2ce40eaee7b6c9daf9e7145c1e2c86ff
JANANI1905 <151322389+JANANI1905@users.noreply.github.com>
2025-12-19 21:41:58 +0530

    Create Valid anagram.java

---
+++ 19.12.25/Valid anagram.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+      HashMap<Character,Integer> map=new HashMap<>();
+      for(int i=0;i<s.length();i++){
+        map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+      }
+      for(int i=0;i<t.length();i++){
+        char c=t.charAt(i);
+        if(!map.containsKey(c) || map.get(c)==0){
+            return false;
+        }
+        map.put(c,map.get(c)-1);
+      }
+
+      for(char ch:map.keySet()){
+        if(map.get(ch)!=0){
+            return false;
+        }
+      }
+      return true;
+
+    }
+}
